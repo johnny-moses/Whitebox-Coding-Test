b655e64e4f1205d2fcce7e9254501bf50882fa98
Johnny Moses <35476759+johnny-moses@users.noreply.github.com>
2020-08-28 22:27:34 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+const mysql = require('mysql');
+const excel = require('exceljs')
+
+// Create connection
+var db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    port:'3306',
+    database:'whitebox'
+});
+
+//connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('Connected to Database')
+    data = db.query("SELECT * FROM rates WHERE id = '1240'", function (err, result, fields) {
+        if (err) throw err;
+       // console.log(result);
+        const jsonData = JSON.parse(JSON.stringify(result));
+        console.log(jsonData)
+
+        let workbook = new excel.Workbook();
+let worksheet = workbook.addWorksheet('Customer');
+
+worksheet.columns = [
+    {header: '_id', key: 'id', width: 10},
+    {header: 'Client_ID', key: 'client_id', width: 10},
+    {header: 'Start_Weight', key: 'start_weight', width: 15},
+    {header: 'End_Weight', key: 'end_weight', width: 15},
+    {header: 'Zone', key: 'zone', width: 10},
+    {header: 'Rate', key: 'rate', width: 10},
+    {header: 'Shipping_Speed', key: 'shipping_speed', width: 20},
+    {header: 'Locale', key: 'locale', width: 10}
+]
+
+worksheet.addRow(jsonData);
+
+workbook.xlsx.writeFile("Customer_Data.xlsx")
+.then(function() {
+    console.log('File Saved')
+})
+
+db.end(function(err){
+    if (err) {
+        return console.log('error' + err.message);
+    }
+    console.log('Database connection closed.')
+})
+    });
+})
+
+
